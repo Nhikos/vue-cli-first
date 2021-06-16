58c6e0f6f605b815b46938b301a21017eddcdba2
Nhikos <4ndre.sergi@gmail.com>
2021-06-16 16:43:30 +0200

    first commit

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Titolo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "Title"
+}
+</script>
+
+
+<style scoped lang="scss">
+h1 {
+  font-size: 40px;
+  }
+</style>
